2a7b28ee48e7a761474bba40cf80bf03bd7df76d
Ziaad-Khaled <ziadkhaled1918@gmail.com>
2023-03-11 17:56:50 +0200

    Task 4 initialized

--- Task 2/Task 2/src/csen1002/main/task2/NfaToDfa.java
+++ Task 2/Task 2/src/csen1002/main/task2/NfaToDfa.java
@@ -260,3 +260,278 @@ public class NfaToDfa {
 	}
 
 }
+class NewState implements Comparable{
+	private ArrayList<Integer>  stateNumbers;
+	private Hashtable<Character, HashSet<Integer>> transitionPairs;
+	private Hashtable<Character, NewState> transitionPairs_state;
+
+	NfaToDfa nfaToDfa;
+
+	public NewState(ArrayList<Integer>  stateNumbers , NfaToDfa nfaToDfa)
+	{
+		this.stateNumbers = stateNumbers;
+		this.transitionPairs_state = new Hashtable<>();
+		if(stateNumbers.isEmpty())
+		{
+			stateNumbers.add(-1);
+		}
+
+		this.nfaToDfa = nfaToDfa;
+		initializeTransitionPairs();
+		fillTransitionPairs();
+	}
+
+	private void initializeTransitionPairs() {
+		transitionPairs = new Hashtable<>();
+		ArrayList<Character> alphabet = nfaToDfa.getInputAlhpabet();
+		for(int i=0;i<alphabet.size();i++)
+		{
+			char symbol = alphabet.get(i);
+			transitionPairs.put(symbol, new HashSet<>());
+		}
+	}
+
+	private void fillTransitionPairs() {
+		ArrayList<TransitionPair> stateTransitionPairs = new ArrayList<>();
+		for(int i=0;i<stateNumbers.size();i++)
+		{
+			if(stateNumbers.get(i) == -1)
+				continue;
+			State state = nfaToDfa.getStateByNumber(stateNumbers.get(i));
+			stateTransitionPairs.addAll(state.getTransitionPairs());
+		}
+		for(int j=0;j<stateTransitionPairs.size();j++)
+		{
+			TransitionPair transitionPair = stateTransitionPairs.get(j);
+			char symbol = transitionPair.getSymbol();
+			if(symbol == 'e')
+				continue;
+			State targetState = transitionPair.getTarget();
+			ArrayList<Integer> epsilonTransitions = targetState.epsilonTransitions;
+			HashSet<Integer> targets = this.transitionPairs.get(symbol);
+			targets.addAll(epsilonTransitions);
+//            System.out.println(symbol);
+//            System.out.println(transitionPair);
+//            System.out.println(this.transitionPairs.toString());
+		}
+	}
+
+	public void addStateTransitionPair(char symbol, NewState newState)
+	{
+		transitionPairs_state.put(symbol, newState);
+	}
+
+	public String stateNumbersToString()
+	{
+		String output = "";
+		for(int i=0; i<stateNumbers.size();i++)
+		{
+			output+= stateNumbers.get(i) + "/";
+		}
+		return output.substring(0, output.length() - 1);
+	}
+
+	public String stateTransitionsToString() {
+		String output = "";
+		ArrayList<Character> keys = new ArrayList<Character>(transitionPairs.keySet());
+		Collections.sort(keys);
+		for (Character key : keys) {
+			output+= this.stateNumbersToString() + ",";
+			output+= key;
+			output += ",";
+			HashSet<Integer> value = transitionPairs.get(key);
+			output+= targetStateToString(value) + ";";
+		}
+		return output.substring(0, output.length() - 1);
+	}
+
+	private String targetStateToString(HashSet<Integer> value) {
+		if(value.size() == 0)
+		{
+			value.add(-1);
+		}
+		String output = "";
+		for (Integer element : value) {
+			output += element.toString() + "/";
+		}
+		return output.substring(0, output.length() - 1);
+	}
+
+	public ArrayList<Integer> getStateNumbers() {
+		return stateNumbers;
+	}
+
+	public Hashtable<Character, HashSet<Integer>> getTransitionPairs() {
+		return transitionPairs;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		NewState newState = (NewState) o;
+		return stateNumbers.equals(newState.stateNumbers);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(stateNumbers, transitionPairs, nfaToDfa);
+	}
+
+
+
+	@Override
+	public String toString() {
+		String output ="state numbers= " + stateNumbers.toString() + "transitionPairs=[";
+		ArrayList<Character> keys = new ArrayList<Character>(transitionPairs.keySet());
+		Collections.sort(keys);
+		for (Character key : keys) {
+			HashSet<Integer> value = transitionPairs.get(key);
+			output+= key.toString() + value.toString() + ",";
+		}
+		return output + "]";
+	}
+
+
+	@Override
+	public int compareTo(Object o) {
+		int sizeDiff = this.getStateNumbers().size() - ((NewState) o).getStateNumbers().size();
+		int size;
+		if(sizeDiff < 0)
+		{
+			size = this.getStateNumbers().size();
+		}
+		else
+		{
+			size = ((NewState) o).getStateNumbers().size();
+		}
+		for (int i = 0; i < size; i++) {
+			int thisElement = this.getStateNumbers().get(i);
+			int otherElement = ((NewState) o).getStateNumbers().get(i);
+			if (thisElement != otherElement) {
+				return thisElement - otherElement;
+			}
+		}
+		if (sizeDiff != 0) {
+			return sizeDiff;
+		}
+		return 0;
+	}
+}
+
+class State {
+
+
+	private NfaToDfa nfaToDfa;
+	private int stateNumber;
+	private ArrayList<TransitionPair> transitionPairs;
+	ArrayList<Integer> epsilonTransitions;
+
+	public State(int stateNumber, NfaToDfa nfaToDfa)
+	{
+		this.stateNumber = stateNumber;
+		this.nfaToDfa = nfaToDfa;
+		transitionPairs = new ArrayList<>();
+		epsilonTransitions = new ArrayList<>();
+		epsilonTransitions.add(this.stateNumber);
+	}
+
+	public void createATransition(char symbol, State target)
+	{
+		TransitionPair transitionPair = new TransitionPair(symbol, target);
+		transitionPairs.add(transitionPair);
+		if(symbol == 'e')
+		{
+			epsilonTransitions.add(target.getStateNumber());
+		}
+	}
+
+	public void fillEpsilonTable()
+	{
+		for(int i=0;i<epsilonTransitions.size();i++)
+		{
+			int targetNumber = epsilonTransitions.get(i);
+			State targetState = nfaToDfa.getStateByNumber(targetNumber);
+			ArrayList<Integer> targetEpsilonTransitions = targetState.getEpsilonTransitions();
+			for(int j=0 ; j< targetEpsilonTransitions.size(); j++)
+			{
+				int targetEpsilonTransition = targetEpsilonTransitions.get(j);
+				if(!epsilonTransitions.contains(targetEpsilonTransition))
+				{
+					epsilonTransitions.add(targetEpsilonTransition);
+				}
+			}
+		}
+		Collections.sort(epsilonTransitions);
+	}
+
+	public int getStateNumber() {
+		return stateNumber;
+	}
+
+	public ArrayList<TransitionPair> getTransitionPairs() {
+		return transitionPairs;
+	}
+
+	public ArrayList<Integer> getEpsilonTransitions() {
+		return epsilonTransitions;
+	}
+
+	public HashSet<State> getTransitionTargets(char symbol)
+	{
+		HashSet<State> targets = new HashSet<>();
+		for(int i=0;i<transitionPairs.size();i++)
+		{
+			if(transitionPairs.get(i).symbol == symbol)
+			{
+				targets.add(transitionPairs.get(i).target);
+			}
+		}
+		return targets;
+	}
+
+	@Override
+	public String toString() {
+		return "transitionPairs=" + transitionPairs +
+				epsilonTransitions.toString();
+	}
+}
+
+class TransitionPair implements Comparable<TransitionPair>{
+	char symbol;
+	State target;
+
+	public TransitionPair(char transition, State target)
+	{
+		this.symbol = transition;
+		this.target = target;
+	}
+
+	public char getSymbol() {
+		return symbol;
+	}
+
+	public State getTarget() {
+		return target;
+	}
+
+	public int getTargetNumber()
+	{
+		return target.getStateNumber();
+	}
+
+	@Override
+	public String toString() {
+		return "transition=" + symbol +
+				", target=" + target.getStateNumber();
+	}
+
+	@Override
+	public int compareTo(TransitionPair other) {
+		return Integer.compare(this.target.getStateNumber(), other.target.getStateNumber());
+	}
+}
+
+
+
+
