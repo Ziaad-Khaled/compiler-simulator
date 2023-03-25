9b0a0cc380d8035be79343d82d2179d01416b26f
Ziaad-Khaled <ziadkhaled1918@gmail.com>
2023-03-25 15:34:13 +0200

    task 6 done

--- Task 6/Task 6/src/csen1002/main/task6/CfgFirstFollow.java
+++ Task 6/Task 6/src/csen1002/main/task6/CfgFirstFollow.java
@@ -1,6 +1,8 @@
 package csen1002.main.task6;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -83,15 +85,37 @@ public class CfgFirstFollow {
 				boolean ruleAdded = variable.assignFirst();
 				if(ruleAdded)
 					change = true;
-				System.out.println(variable.getName());
-				System.out.println(variable.getFirst());
-				System.out.println("#################");
 			}
-			System.out.println("$$$$$$$$$$");
 		}
 
 		return firstToString();
 	}
+
+
+	/**
+	 * Calculates the Follow Set of each variable in the CFG.
+	 * 
+	 * @return A string representation of the Follow of each variable in the CFG,
+	 *         formatted as specified in the task description.
+	 */
+	public String follow() {
+		first();
+		boolean change = true;
+		while(change)
+		{
+			change = false;
+			for(int i=0;i< variables.size();i++)
+			{
+				Variable variable = variables.get(i);
+				boolean ruleAdded = variable.assignFollow();
+				if(ruleAdded)
+					change = true;
+			}
+		}
+
+		return followToString();
+	}
+
 
 	private String firstToString() {
 		String output = "";
@@ -110,16 +134,21 @@ public class CfgFirstFollow {
 		return output;
 	}
 
+	private String followToString() {
+		String output = "";
+		for(int i=0;i<variables.size();i++)
+		{
+			Variable variable = variables.get(i);
+			output += variable.getName();
+			output += "/";
+			output += variable.followToString();
+			output += ";";
+		}
 
-	/**
-	 * Calculates the Follow Set of each variable in the CFG.
-	 * 
-	 * @return A string representation of the Follow of each variable in the CFG,
-	 *         formatted as specified in the task description.
-	 */
-	public String follow() {
-		// TODO Auto-generated method stub
-		return null;
+		if(output.charAt(output.length()-1) == ';')
+			output = output.substring(0, output.length() - 1);
+
+		return output;
 	}
 
 	public Variable getVariableByName(Character character)
@@ -134,5 +163,223 @@ public class CfgFirstFollow {
 
 		return null;
 	}
+
+}
+
+class Variable {
+
+	private CfgFirstFollow cfgFirstFollow;
+	private Character name;
+	private ArrayList<String> rules;
+	private HashSet<Character> first;
+	private HashSet<Character> follow;
+
+	public Variable(CfgFirstFollow cfgFirstFollow, Character name)
+	{
+		this.cfgFirstFollow = cfgFirstFollow;
+		this.name = name;
+		this.rules = new ArrayList<>();
+		this.first = new HashSet<>();
+		this.follow = new HashSet<>();
+
+		if(name == 'S')
+		{
+			follow.add('$');
+		}
+
+	}
+
+	public void addRule(String rule)
+	{
+		rules.add(rule);
+	}
+
+	public boolean assignFirst()
+	{
+		int oldSize = first.size();
+		for(int i=0;i<rules.size();i++)
+		{
+			String rule = rules.get(i);
+			boolean allEpsilon = true;
+			for(int j=0; j< rule.length();j++)
+			{
+				Character character = rule.charAt(j);
+				ArrayList<Character> targetFirst = firstHelper(character);
+				if(!targetFirst.contains('e'))
+				{
+					allEpsilon = false;
+					break;
+				}
+			}
+			if(allEpsilon)
+				first.add('e');
+		}
+
+		for(int i=0;i<rules.size();i++)
+		{
+			String rule = rules.get(i);
+			ArrayList<Character> targetFirst = new ArrayList<>();
+			for(int j=0; j< rule.length();j++)
+			{
+				Character character = rule.charAt(j);
+				targetFirst = firstHelper(character);
+				boolean hadEpsilon = targetFirst.contains(Character.valueOf('e'));
+				targetFirst.remove(Character.valueOf('e'));
+				first.addAll(targetFirst);
+				if(!hadEpsilon)
+					break;
+			}
+		}
+
+		return oldSize != first.size();
+	}
+
+
+	public boolean assignFollow()
+	{
+		boolean change = false;
+		for(int i=0;i<rules.size();i++) {
+			String rule = rules.get(i);
+			for(int j=0; j < rule.length() - 1;j++)
+			{
+				Character character = rule.charAt(j);
+				String followingCharacters = rule.substring(j+1);
+				if(Character.isUpperCase(character))
+				{
+					boolean changed = assignFollowHelper(character, followingCharacters);
+					if(changed)
+						change = true;
+				}
+			}
+		}
+
+		for(int i=0;i<rules.size();i++) {
+			String rule = rules.get(i);
+			boolean changed = assignFollowHelperSecondCase(rule);
+			if(changed)
+				change = true;
+		}
+		return change;
+	}
+
+
+
+	private boolean assignFollowHelper(Character character, String followingCharacters) {
+		boolean change = false;
+		Variable variable = cfgFirstFollow.getVariableByName(character);
+		Character followingCharacter = followingCharacters.charAt(0);
+		boolean changed = false;
+		if(Character.isLowerCase(followingCharacter))
+		{
+			change = variable.addFollowCharacter(followingCharacter);
+		}
+		else
+		{
+			Variable followingVariable = cfgFirstFollow.getVariableByName(followingCharacter);
+			HashSet<Character> followingVariableFirst = followingVariable.getFirst();
+			HashSet<Character> followingVariableFirst_copy = new HashSet<>(followingVariableFirst);
+
+			if(followingVariableFirst.contains('e') && followingCharacters.length() > 1)
+			{
+				String subString = followingCharacters.substring(1);
+				changed = assignFollowHelper(character, subString);
+			}
+
+			if(changed)
+				change = true;
+			followingVariableFirst_copy.remove(Character.valueOf('e'));
+			changed = variable.addFollowCharacter(followingVariableFirst_copy);
+			if(changed)
+				change = true;
+		}
+
+		return change;
+	}
+
+	private boolean assignFollowHelperSecondCase(String rule) {
+		boolean change = true;
+		Character lastCharacter = rule.charAt(rule.length()-1);
+		if(Character.isLowerCase(lastCharacter))
+			return false;
+		Variable variable = cfgFirstFollow.getVariableByName(lastCharacter);
+		return variable.addFollowCharacter(this.follow);
+	}
+
+	public ArrayList<Character> firstHelper(Character character)
+	{
+		ArrayList<Character> first = new ArrayList<>();
+		if(Character.isLowerCase(character))
+			first.add(character);
+		else
+		{
+			Variable variable = cfgFirstFollow.getVariableByName(character);
+			first.addAll(variable.getFirst());
+		}
+		return first;
+	}
+
+	public boolean addFollowCharacter(Character character)
+	{
+		int oldSize = follow.size();
+		follow.add(character);
+		return oldSize != follow.size();
+	}
+
+	public boolean addFollowCharacter(HashSet<Character> characters)
+	{
+		int oldSize = follow.size();
+		follow.addAll(characters);
+		return oldSize != follow.size();
+	}
+
+
+
+	public HashSet<Character> getFirst()
+	{
+		return this.first;
+	}
+
+
+	public Character getName() {
+		return name;
+	}
+
+	public String firstToString()
+	{
+		String output = "";
+		ArrayList<Character> first_arr = new ArrayList<Character>(first);
+		Collections.sort(first_arr);
+		for(int i=0;i<first.size();i++)
+		{
+			output += first_arr.get(i);
+		}
+		return output;
+	}
+
+	public String followToString() {
+		String output = "";
+		ArrayList<Character> follow_arr = new ArrayList<Character>(follow);
+		Collections.sort(follow_arr);
+		for(int i=0;i<follow.size();i++)
+		{
+			output += follow_arr.get(i);
+		}
+		return output;
+	}
+
+	@Override
+	public String toString() {
+		String output = name + "";
+		output += "/";
+		for(int i=0;i<rules.size();i++)
+		{
+			output+= rules.get(i);
+			output += ",";
+		}
+		if(output.charAt(output.length()-1) == '/')
+			output += "/";
+		return  output.substring(0, output.length()-1);
+	}
+
 
 }
